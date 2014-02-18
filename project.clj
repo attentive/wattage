7fd71fc06f12c09d7cfa61a466223f6ba8f652d6
tflynch <tflynch@gmail.com>
2014-02-18 23:11:59 +0800

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject wattage "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Wattage is an ID3 tag viewer based on David Nolen's Om (Facebook React)."
+  :url "http://github.com/attentive/wattage"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
@@ -13,7 +13,7 @@
                  [om "0.4.0"]
                  [compojure "1.1.6"]
                  [fogus/ring-edn "0.2.0"]
-                 [com.datomic/datomic-free "0.9.4532"]
+                 ; Note: this dependency is not currently distributed centrally.
                  [wat/wat "0.2.0"]]
 
   :plugins [[lein-cljsbuild "1.0.2"]]
